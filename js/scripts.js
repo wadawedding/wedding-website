2776690fec2a9009384a1a5c58e7fb37600b8abd
jkaw <jwada@westmont.edu>
2023-02-28 00:23:41 -0800

    update add calendar feature

--- js/scripts.js
+++ js/scripts.js
@@ -186,23 +186,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Clare and Jared's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('October 7, 2023 17:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('October 7, 2023 23:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '1250 E. Harvard RD. Burbank, CA 91510-1096 ',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day."
         }
     });
 
