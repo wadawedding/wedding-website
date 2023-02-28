1d02b78a5793139d25d097a1ee87994a5514c793
jkaw <jwada@westmont.edu>
2023-02-27 16:50:02 -0800

    update rsvp page

--- js/scripts.js
+++ js/scripts.js
@@ -216,11 +216,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'b5af41b269f7d4efc530e7a95cba9750'
+            && MD5($('#invite_code').val()) !== 'eb0ae6e38cbfc7239a807dd973690bfa') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxyIkzUM5fpzdg9rHZFK3zzV-k7pQdPenCTzN0MAr_6f0SOIWrfJasjJjPdTZjyf1cU/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
